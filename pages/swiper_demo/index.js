649838f7c0761ba1db3fc25369c676b169b0421d
flyccyy <13413538434@163.com>
2019-11-09 15:02:04 +0800

    second

--- pages/swiper_demo/index.js
+++ pages/swiper_demo/index.js
@@ -1,4 +1,4 @@
-// pages/switch_images/index.js
+// pages/swiper_demo/index.js
 Page({
 
   /**
@@ -6,33 +6,11 @@ Page({
    */
   data: {
     imgList:[
-      // /images/boy.png 中的第一个/是代表根路径，即绝对路径
-      '../../images/boy.png',
-      '../../images/girl.png',
-      '../../images/left.gif',
-      '../../images/right.png',
-    ],
-    index:0,
-  },
-  // 上一页的点击事件
-  prevTap(){
-    let index = --this.data.index;
-    if(index === -1){
-      index = this.data.imgList.length-1;
-    }
-    this.setData({
-      index: index
-    })
-  },
-  // 下一页的点击事件
-  nextTap(){
-    let index = ++this.data.index;
-    if(index === this.data.imgList.length){
-      index = 0;
-    }
-    this.setData({
-      index:index
-    })
+      '/images/boy.png',
+      '/images/girl.png',
+      '/images/left.gif',
+      '/images/right.png',
+    ]
   },
 
   /**
